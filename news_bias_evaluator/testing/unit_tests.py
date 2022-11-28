b92e5478adb1dd352ec7d189c12c43bb9e874b5c
VernitaJ <gusgouve@student.gu.se>
2022-11-28 11:49:09 +0100

    Add unit test initialisation to dockerfile in news_evaluator

---
+++ news_bias_evaluator/testing/unit_tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+# TODO(VG): Add unit tests for the news_bias_evaluator package.
+
+class TestMethods(unittest.TestCase):
+    # placeholder test
+    def test_article_column(self):
+        for i in range(0,100):
+            self.assertIsInstance('this is a string', str)
+    
+if __name__ == '__main__':
+    unittest.main()
