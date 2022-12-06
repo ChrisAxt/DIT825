4d4afa0edccd9a0db00bebf7d70e1d432ed42135
Alexandre Rancati-Palmer <aleran@student.chalmers.se>
2022-12-06 12:33:01 +0100

    Frontend: Added comment for clarity #AP-69

--- news_bias_evaluator/client/views.py
+++ news_bias_evaluator/client/views.py
@@ -7,9 +7,11 @@ def index(request):
 
 def results(request):
     
-    text_input = request.GET['input-text'] # retrieve the text input
+    text_input = request.GET['input-text'] # retrieve the text input from form 
 
     sentences = {text_input,}
+
+    # creates a context (dictionary mapping variables to HTML variables)
     context = {
         'sentences': sentences
     }
