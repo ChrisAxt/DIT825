6e73d48415288d01867ef4fd3d68ee2759aa1822
Alexandre Rancati-Palmer <aleran@student.chalmers.se>
2022-12-06 12:22:13 +0100

    Frontend: View for results after model processing - Deactivated checkbox (just need to be commented in when we implement   feature) - Use get method for now since we are not changing data. Will need to   change to post when saving user input issue #AP-69

--- news_bias_evaluator/client/urls.py
+++ news_bias_evaluator/client/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('results', views.results, name='results')
 ]
--- news_bias_evaluator/client/views.py
+++ news_bias_evaluator/client/views.py
@@ -3,5 +3,14 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    template = loader.get_template('client/index.html')
-    return HttpResponse(template.render())
+    return render(request, 'client/index.html')
+
+def results(request):
+    
+    text_input = request.GET['input-text'] # retrieve the text input
+
+    sentences = {text_input,}
+    context = {
+        'sentences': sentences
+    }
+    return render(request, 'client/results.html', context)
