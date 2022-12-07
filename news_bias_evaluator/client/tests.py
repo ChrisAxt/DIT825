7682e4c94dc59ac622c66db7ff272f17cf505f80
Chris Axt <gusaxtcha@student.gu.se>
2022-12-07 19:32:58 +0100

    AP-36: unit tests + onSubmit function

--- news_bias_evaluator/client/tests.py
+++ news_bias_evaluator/client/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+import unittest
 
-# Create your tests here.
+from utils import * 
+
+class UserInputTestCase(unittest.TestCase):
+    def setUp(self):
+        self.testInput = "This is my test string. What do you think of it? I think it's awesome!"
+        self.sentenceList = extractSentences(self.testInput)
+
+    def test_extractSentences_period(self):
+        self.assertEqual(self.sentenceList[0], "This is my test string." ,"Failed to extract sentence ending in period")
+        
+    def test_extractSentences_question(self):
+        self.assertEqual(self.sentenceList[1], "What do you think of it?" ,"Failed to extract sentence ending in question mark")
+
+    def test_extractSentences_exclamation(self):
+        self.assertEqual(self.sentenceList[2], "I think it's awesome!" ,"Failed to extract sentence ending in exclamation mark")
+
+    def test_sendRequest(self):
+        predictionList = sendRequest(self.sentenceList)
+        self.assertEqual(len(self.sentenceList),  len(predictionList['predictions']), "Failed to send the request to the model")
--- news_bias_evaluator/client/views.py
+++ news_bias_evaluator/client/views.py
@@ -2,17 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from news_bias_evaluator.client.utils import extractSentences, sendRequest
+
+
 def main(request):
     return render(request, 'client/main.html')
 
-def results(request):
-    
+def onSubmit(request):
     text_input = request.GET['input-text'] # retrieve the text input from form 
+    sentenceList = extractSentences(text_input)
+    if(len(sentenceList) > 0):
+        predictionList = sendRequest(sentenceList)
 
-    sentences = {text_input,}
+    if (len(sentenceList) > 0 & len(sentenceList) == len(predictionList)):
+        result = {sentenceList, predictionList}
 
     # creates a context (dictionary mapping variables to HTML variables)
     context = {
-        'sentences': sentences
+        'result': result
     }
+
     return render(request, 'client/results.html', context)
+
+
