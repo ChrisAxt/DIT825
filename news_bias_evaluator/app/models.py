1171f4e406f547d98913b4059372bacd5cb4b1e4
Alexandre Rancati-Palmer <aleran@student.chalmers.se>
2022-12-14 16:30:02 +0100

    Backend: schemas for DB + changed initial upload script to match #AP-80

--- news_bias_evaluator/app/models.py
+++ news_bias_evaluator/app/models.py
@@ -1,22 +1,63 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField 
+import datetime
 
-# Create your models here.
-# Model for enrty
-class initialEntry(models.Model):
-    sentence = models.TextField(primary_key=True)
-    news_link = models.CharField(max_length=200)
+# Helper methods to creates choices for years and date values.
+def current_date():
+    '''
+    returns the current date
+    '''
+    return datetime.date.today()
+
+def current_year():
+    '''
+    returns the current year
+    '''
+    return datetime.date.today().year
+
+def possible_years():
+    '''
+    returns a list of choices for publication years
+    Starting date is arbitrary
+    Inspired by: https://stackoverflow.com/questions/49051017/year-field-in-django
+    '''
+    return [(r,r) for r in range(1984, current_year)]
+
+
+# MODELS
+
+class Article(models.Model):
+    '''
+    Model for an article. 
+    It contains some attributes that will be used by the model but that are related to the article and not the sentence. 
+    Use foreign key to retreive those attributes.
+    '''
+    news_link = models.CharField(max_length=200, primary_key=True)
+    article = models.TextField()
     outlet = models.CharField(max_length=20)
     topic = models.CharField(max_length=20)
     political_type = models.CharField(max_length=10)
-    group_id = models.IntegerField()
-    num_sent = models.IntegerField()
+    pub_year = models.IntegerField(null=True)
+    add_date = models.DateField(default=current_date)
+    
+        # Returned when a particular article is queried
+    def __str__(self):
+        return "Article's URL: ", self.news_link
+
+
+class LabeledSentence(models.Model):
+    '''
+    Model for labeled sentences
+    To retrieve articles that have not their sentences labeled, we can query the DB
+    Is contected vie foreign key to an article. This allows granularity when querying DB based on date, etc.
+    We can also retrieve the entire article to give context to the sentence, allowing more advanced training in the futur. 
+    '''
+    sentence = models.TextField(primary_key=True)
     label_bias = models.CharField(max_length=10)
     label_opinion = models.CharField(max_length=50)
-    article = models.TextField()
-    # Bias words added as a textfield as SQlite does not support array's.
-    # This will have to be seperated using a regex function if we contiune with SQlite
-    bias_words = models.TextField()
+    bias_words = ArrayField(models.CharField(max_length=30, blank=True))
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, max_length=200)  #if an article is deleted, the sentence will be deleted as well
     
-    # Returned when a particular entry is quaried
+    # Returned when a particular sentence is queried
     def __str__(self):
         return self.sentence
--- news_bias_evaluator/app/scripts/reset_and_load_initial_training_data.py
+++ news_bias_evaluator/app/scripts/reset_and_load_initial_training_data.py
@@ -1,4 +1,6 @@
-from app.models import initialEntry
+from app.models import Article, LabeledSentence
+
+
 import csv
 
 # Script to extract a cleaned version of the dataset (unnecessary columns and rows removed)
@@ -9,27 +11,31 @@ def run():
         next(reader)  # Advance past the header
 
         # Delete all entries from the database
-        initialEntry.objects.all().delete()
+        Article.objects.all().delete()
 
         # Itterate over each row in the csv file
         for row in reader:
             print(row)
             
             # Extract the data at the relevent positions
-            entry = initialEntry(
-                sentence=row[0],
+            new_article = Article(
                 news_link=row[1],
                 outlet=row[2],
                 topic=row[3],
                 political_type=row[4],
-                group_id=row[5],
-                num_sent=row[6],
+                article=row[9],
+               )
+
+            new_sentence = LabeledSentence(
+                sentence=row[0],
+                bias_words=row[10],
                 label_bias=row[7],
                 label_opinion=row[8],
-                article=row[9],
-                bias_words=row[10],)
+                article = row[1],
+            )
 
             # Save each entry to the database
-            entry.save()
+            new_article.save()
+            new_sentence.save()
 
 
