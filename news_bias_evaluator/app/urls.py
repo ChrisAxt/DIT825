4a3556c2040770be4f0ec96e96c06896dfc9c5e6
Alexandre Rancati-Palmer <aleran@student.chalmers.se>
2022-12-22 11:49:25 +0100

    Frontend: Implemented admin UI and placeholder as described in #AP-99

--- news_bias_evaluator/app/urls.py
+++ news_bias_evaluator/app/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
+app_name = 'app'
 urlpatterns = [
     path('', views.main, name='main'),
-    path('results', views.onSubmit, name='results')
+    path('results/', views.onSubmit, name='results'),
+    path('admin/', views.dispatch, name='dispatch'),
+    path('login/', views.log, name='login'),
+    path('logout/', views.log_out, name='logout'),
+    path('admin/dashboard/', views.access_dashboard, name="dashboard"),
+    path('admin/dashboard/process_admin_request/', views.process_admin_request, name="process_admin_request"),
+    path('admin/database/', admin.site.urls, name='database')
 ]
