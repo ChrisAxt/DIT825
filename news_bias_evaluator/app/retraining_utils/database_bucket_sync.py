686599548e93d31c4dae5ffc17900d1382cd9b33
younisa <younisa@student.chalmers.se>
2022-12-30 11:41:25 +0300

    AP-96 Add database to cloud bucket sync

--- news_bias_evaluator/app/retraining_utils/database_bucket_sync.py
+++ news_bias_evaluator/app/retraining_utils/database_bucket_sync.py
@@ -1,14 +1,31 @@
 # Shall convert data from cloudSQL into a csv format for the ai platform bucket.
 from app.models import LabeledSentence
+import pandas as pd
+from validator import prepare_training_data 
+from google.cloud import storage
 
-def get_data_from_db(query_params):
-    db_data = LabeledSentence.objects.all().values()
-    print(db_data)
-    return db_data
-    # Pull in data from database
-    # validate the data
+def sync_db_and_bucket(): 
+    # Pull data from database
+    db_data_df = get_data_from_db()
+    # Validate and clean the data
+    training_data_df = prepare_training_data(db_data_df)
+    # Convert to csv and store to the bucket
+    store_data_to_bucket(training_data_df)
 
-#def convert_to_csv(data):
-    # convert data to csv
 
-#def store_data_to_bucket():
+def get_data_from_db():
+    db_data_df = pd.DataFrame(list(LabeledSentence.objects.all().values()))
+    print(db_data_df)
+    return db_data_df
+    
+
+def store_data_to_bucket(training_data_df):
+    
+    storage_client = storage.Client()
+    bucket = storage_client.bucket('example_bucket_v2-aiproject-dit825')
+    blob = bucket.blob('training_data/')
+
+    blob.upload_from_string(convert_to_csv(training_data_df), 'text/csv')
+
+def convert_to_csv(db_data_df):
+    return db_data_df.to_csv()
