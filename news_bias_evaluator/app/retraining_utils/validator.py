b750592e56d60fb966105c1c9ab99b2d9656eb56
younisa <younisa@student.chalmers.se>
2022-12-30 11:40:18 +0300

    AP-96 Add training data cleaning/validator

--- news_bias_evaluator/app/retraining_utils/validator.py
+++ news_bias_evaluator/app/retraining_utils/validator.py
@@ -1,1 +1,14 @@
+import pandas as pd
+
 # Validate incoming data before storing into cloudSQL.
+def prepare_training_data(db_data_df: pd.DataFrame):
+    # Select only wanted columns
+    training_data_df = db_data_df[["sentence", "label_bias"]]
+    
+    # Remove no agreement labels
+    training_data_df = training_data_df[training_data_df.Label_bias != 'No agreement']
+
+    # Remove rows with missing values 
+    training_data_df = training_data_df.dropna()
+
+    return training_data_df
