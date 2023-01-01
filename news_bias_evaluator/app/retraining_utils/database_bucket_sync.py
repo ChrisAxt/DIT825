f5921aabd036065654b6ffe9541ea4c44202b5f5
younisa <younisa@student.chalmers.se>
2023-01-01 11:49:49 +0300

    AP-96 Add evaluation data to database bucket sync

--- news_bias_evaluator/app/retraining_utils/database_bucket_sync.py
+++ news_bias_evaluator/app/retraining_utils/database_bucket_sync.py
@@ -6,17 +6,26 @@ from google.cloud import storage
 
 def sync_db_and_bucket(): 
     # Pull data from database
-    db_data_df = get_data_from_db()
+    db_training_data_df, db_evaluation_data_df = get_data_from_db()
     # Validate and clean the data
-    training_data_df = validator.prepare_training_data(db_data_df)
+    training_data_df = validator.prepare_data(db_training_data_df)
+    evaluation_data_df = validator.prepare_data(db_evaluation_data_df) 
     # Convert to csv and store to the bucket
-    store_data_to_bucket(training_data_df, 'example_bucket_v2-aiproject-dit825', 'training_data/media_bias_dataset_cleaned.csv')
+    bucket_name = 'example_bucket_v2-aiproject-dit825'
+    store_data_to_bucket(training_data_df, bucket_name, 'training_data/media_bias_dataset_cleaned.csv')
+    store_data_to_bucket(evaluation_data_df, bucket_name, 'evaluation_data/evaluation_data.csv')
 
 
 def get_data_from_db():
-    db_data_df = pd.DataFrame(list(LabeledSentence.objects.all().values()))
-    print(db_data_df)
-    return db_data_df
+    db_training_data_df = pd.DataFrame(list(LabeledSentence.objects.all().values()))
+    # Remove last 50 entries to train the model on.
+    # (ie dont train model on eval data)
+    db_training_data_df = db_training_data_df[:-50 or None]
+    # Get the last 50 values for evaluation
+    db_eval_data_df = pd.DataFrame(list(LabeledSentence.objects.all()[-50:].values()))
+    print(db_training_data_df)
+    print(db_eval_data_df)
+    return db_training_data_df, db_eval_data_df
     
 
 def store_data_to_bucket(data_df, bucket_name, bucket_file):
