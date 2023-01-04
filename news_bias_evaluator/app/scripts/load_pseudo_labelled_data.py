6f77ec4e5a9ca975930202a04383819f8e61798f
Alexandre Rancati-Palmer <aleran@student.chalmers.se>
2023-01-04 08:24:11 +0000

    Unit-test: implemented methods to check values before creating articles and labeled sentence

--- news_bias_evaluator/app/scripts/load_pseudo_labelled_data.py
+++ news_bias_evaluator/app/scripts/load_pseudo_labelled_data.py
@@ -1,35 +1,54 @@
 from app.models import Article, LabeledSentence
+from app.utils import *
 
 import csv
 
 # Script to add the pseudo labelled data to the database
 def run():
     with open('app/assets/pseudo_labelled.csv', errors="ignore") as file:
-        reader = csv.reader(file)
-        next(reader)  # Advance past the header
+        reader = csv.DictReader(file, delimiter=',')
 
         # Iterate over each row in the csv file
-        count = 0;
+        valid_count = 0;
+        invalid_count = 0;
         print("Processing CSV \n")
         for row in reader:
-                        
-            # Extract the data at the relevent positions
-            new_article = Article(
-                news_link=row[2],
-                outlet=row[3],
-               )
-            new_article.save()
 
-            new_sentence = LabeledSentence(
-                sentence=row[0],
-                label_bias=row[5],
-                article = new_article,
-            )
-            new_sentence.save()
-            count += 1
-        print("Added rows: ", count)
-        # Save each entry to the database
-            
+            # Extract the data at the relevent positions
+            # TODO: Find a way to check if header is present to be able to use only 1 script to load data
+
+            news_link = row['news_link']
+            outlet=row['outlet']
+            sentence=row['sentence']
+            label_bias=row['label']
+            pub_year = row['pub_year']
+
+            # checks if data extracted is valid
+            if(is_valid_news_link(news_link) and is_non_empty_sentence(sentence) and is_valid_label_bias(label_bias)):
+                
+                # creates an article
+                new_article = Article(
+                    news_link=news_link,
+                    outlet=outlet,
+                    pub_year=pub_year,
+                )
+                new_article.save()
+
+                # creates a sentence
+                new_sentence = LabeledSentence(
+                    sentence=sentence,
+                    label_bias=convert_label_bias(label_bias), # converts the label_bias to number format
+                    article = new_article,
+                )
+                new_sentence.save()
+                valid_count += 1
+                print("Added rows: ", valid_count)
+            else:
+                invalid_count += 1
+
+        print("Total valid rows: ", valid_count)
+        print("Total invalid rows: ", invalid_count)
+                  
 
 
 
--- news_bias_evaluator/app/tests.py
+++ news_bias_evaluator/app/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 import unittest
 
 from .utils import * 
@@ -24,6 +24,72 @@ class UserInputTestCase(unittest.TestCase):
     #    predictionList = sendRequest(self.sentenceList)
     #    self.assertEqual(len(self.sentenceList),  len(predictionList['predictions']), "Failed to send the request to the model")
 
+
+class Article_and_sentence_validation(TestCase):
+    '''
+    Test suite for the methods checking important attributes for Article and LabeledSentence 
+    when created via import of CSV file. 
+    '''
+
+    def test_news_link_is_valid(self):
+        news_link_test = "https://www.freecodecamp.org/news/python-lowercase-how-to-use-the-string-lower-function/"
+        self.assertTrue(is_valid_news_link(news_link_test))
+
+    def test_news_link_is__not_valid(self):
+        news_link_test = "freecodecamp.org/news/python-lowercase-how-to-use-the-string-lower-function"
+        self.assertFalse(is_valid_news_link(news_link_test))
+    
+    def test_sentence_is_not_empty_should_be_true(self):
+        sentence_test = "This is a sentence!"
+        self.assertTrue(is_non_empty_sentence(sentence_test))
+
+    def test_sentence_is_empty_should_be_false(self):
+        sentence_test = ""
+        self.assertFalse(is_non_empty_sentence(sentence_test))
+
+    def test_sentence_is_blank_should_be_false(self):
+        sentence_test = "  "
+        self.assertFalse(is_non_empty_sentence(sentence_test))
+
+    def test_is_valid_label_bias_should_be_true(self):
+        label_bias_test = "BIASED"
+        self.assertTrue(is_valid_label_bias(label_bias_test))
+
+    def test_is_valid_label_bias_should_be_true(self):
+        label_bias_test = "non-BIASED"
+        self.assertTrue(is_valid_label_bias(label_bias_test))
+    
+    def test_is_not_valid_label_bias_should_be_false(self):
+        label_bias_test = "No agreement"
+        self.assertFalse(is_valid_label_bias(label_bias_test))
+    
+    def test_is_empty_label_bias_should_be_false(self):
+        label_bias_test = ""
+        self.assertFalse(is_valid_label_bias(label_bias_test))
+    
+    def test_is_0_label_bias_should_be_true(self):
+        label_bias_test = "0"
+        self.assertTrue(is_valid_label_bias(label_bias_test))
+
+    def test_is_1_label_bias_should_be_true(self):
+        label_bias_test = "1"
+        self.assertTrue(is_valid_label_bias(label_bias_test))
+
+    def test_convert_label_bias_should_be_0(self):
+        label_bias_test = "Non-biAsed"
+        self.assertEqual(convert_label_bias(label_bias_test), '0')
+
+    def test_convert_label_bias_should_be_1(self):
+        label_bias_test = "BiaseD"
+        self.assertEqual(convert_label_bias(label_bias_test), '1')
+
+    def test_convert_label_bias_should_be_0(self):
+        label_bias_test = "0"
+        self.assertEqual(convert_label_bias(label_bias_test), '0')
+
+    def test_convert_label_bias_should_be_1(self):
+        label_bias_test = "1"
+        self.assertEqual(convert_label_bias(label_bias_test), '1')
     
 if __name__ == '__main__':
     unittest.main()
